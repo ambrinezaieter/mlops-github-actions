f307795fb41b8288ae1149931129a127b2c4aa07
ambrinezaieter <ambrine.zaieter@gmail.com>
2026-05-29 15:36:42 +0200

    feat(calculator): add caculator module

---
+++ src/calculator/operations.py
@@ -0,0 +1,22 @@
+def add(a: float, b: float) -> float:
+    return a + b
+
+
+def subtract(a: float, b: float) -> float:
+    return a - b
+
+
+def multiply(a: float, b: float) -> float:
+    return a * b
+
+
+def divide(a: float, b: float) -> float:
+    if b == 0:
+        raise ValueError("Division by zero is not allowed")
+    return a / b
+
+
+print(add(5, 3))  # Output: 8
+print(subtract(5, 3))  # Output: 2
+print(multiply(5, 3))  # Output: 15
+print(divide(5, 3))  # Output: 1.666666666
